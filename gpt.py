72ee4f034e97c66fc3f63eac8d9108ceba7bf469
Ranjan <lamsalranjan19@gmail.com>
2024-02-29 21:44:23 +0545

    PandasAI and Streamlit updated

--- gpt.py
+++ gpt.py
@@ -1,16 +1,28 @@
 import streamlit as st
-from langchain_community.document_loaders import csv_loader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain_community.llms import openai
-from langchain_community.chat_models import ChatOpenAI
-from langchain.agents.agent_types import AgentType
-from langchain_experimental.agents.agent_toolkits import create_csv_agent
-from langchain_openai import ChatOpenAI, OpenAI
+import pandas as pd
+from pandasai import SmartDatalake
+from datetime import datetime
+from pandasai.llm import OpenAI
 from dotenv import load_dotenv
 
 def main():
+
+    # Load environment variables from .env file
     load_dotenv()
 
+    #OpenAI language learning model
+    llm = OpenAI(api_token="your-api-key")
+
+    #Importing Necessary Dataframes
+    sku_data = pd.read_csv("./sku_data.csv")
+    country_id_mapping = pd.read_csv("./country_id_mapping.csv")
+    session_clickstream_data = pd.read_csv("./session_clickstream_data.csv")
+    customer_data = pd.read_csv("./customer_data.csv")
+
+    #For Multiple Dataset Using DataLake
+    df = SmartDatalake([sku_data, country_id_mapping, session_clickstream_data, customer_data], config={"llm": llm})
+
+    #Streamlit page configuration
     st.set_page_config(page_title="CSV GPT")
     st.header("CSV GPT")
 
@@ -19,44 +31,55 @@ def main():
          ('Easy', 'Medium', 'Hard'))
 
     if option == "Easy":
-        st.button(
-            "In which month was the most products sold?", use_container_width=False)
+        if st.button(
+            "In which month was the most products sold?", use_container_width=False):
+            response = df.chat("In Which month was the most product sold")
+            st.write(response)
+
     
     elif option == "Medium":
-        st.button(
-            "What are the most common sizes available for the top-selling products?",use_container_width=False)
-        st.button(
-            "What is the average spend in the summer vs spring?",use_container_width=False)
-        st.button(
-            "Identify top customers with high purchases and determine what is their common frequency to buy?",use_container_width=False)
-        st.button(
-            "What is the most common color amongst the different clothing types?",use_container_width=False)
-        
+        if st.button(
+            "What are the most common sizes available for the top-selling products?",use_container_width=False):
+            response = df.chat("What are the most common sizes available for the top-selling products")
+            st.write(response)
+
+        if st.button(
+            "What is the average spend in the summer vs spring?",use_container_width=False):
+            response = df.chat("What is the average spend in the summer vs spring")
+            st.write(response)
+
+        if st.button(
+            "Identify top customers with high purchases and determine what is their common frequency to buy?",use_container_width=False):
+            response = df.chat("Identify top customers with high purchases and determine what is their common frequency to buy")
+            st.write(response)
+
+        if st.button(
+            "What is the most common color amongst the different clothing types?",use_container_width=False):
+            response = df.chat("What is the most common color amongst the different clothing types")
+            st.write(response)
+
     else:
-        st.button(
-            "How many products does a customer browse before making a purchase on weekends and holidays vs weekdays?",use_container_width=False)
-        st.button(
-            "What were the 5 most common product colors sold in Europe in May, June, and July?",use_container_width=False)
-        st.button(
-            "Are there any trends in product features (texture, color, type) and the session activity (time spent, avg purchases, etc.)?",use_container_width=False)
+        if st.button(
+            "How many products does a customer browse before making a purchase on weekends and holidays vs weekdays?",use_container_width=False):
+            response = df.chat("How many products does a customer browse before making a purchase on weekends and holidays vs weekdays")
 
+        if st.button(
+            "What were the 5 most common product colors sold in Europe in May, June, and July?",use_container_width=False):
+            response = df.chat("What were the 5 most common product colors sold in Europe in May, June, and July")
+            st.write(response)
 
-        
+        if st.button(
+            "Are there any trends in product features (texture, color, type) and the session activity (time spent, avg purchases, etc.)?",use_container_width=False):
+            response = df.chat("Are there any trends in product features (texture, color, type) and the session activity (time spent, avg purchases, etc.)")
+            st.write(response)
 
         
 
     user_prompt = st.text_input("Enter your Prompt")
+    response = df.chat(user_prompt)
+    st.write(response)
 
-    agent = create_csv_agent(
-    ChatOpenAI(temperature=0, model="gpt-3.5-turbo"),
-    ["country_id_mapping.csv", "customer_data.csv", "session_clickstream_data.csv", "sku_data.csv"],
-    verbose=True,
-    agent_type=AgentType.OPENAI_FUNCTIONS,
-    )
-    
-    if user_prompt is not None and user_prompt != "":
-        response = agent.run(user_prompt)
-        st.write(response)
-         
+
+
 if __name__ == "__main__":
     main()
